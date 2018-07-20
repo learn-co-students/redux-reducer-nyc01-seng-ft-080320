6ffa1a46aeb79964d84f7ea8b2754cc44f5833fc
maxwell <github email address>
2018-07-20 14:13:40 -0400

    updated solution to use spread operators

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,21 +4,25 @@ export function manageFriends(state = {
   switch(action.type) {
 
     case "ADD_FRIEND":
-      return Object.assign({}, state, {
-        friends: [
-          ...state.friends,
-          action.friend
-        ]
-      });
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    )
 
     case "REMOVE_FRIEND":
       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-      return Object.assign({}, state, {
-        friends: [
-          ...state.friends.slice(0, removalIndex),
-          ...state.friends.slice(removalIndex + 1)
-        ]
-      });
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
 
     default:
       return state;
