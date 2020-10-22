68933c64e2f5975fb0186b83c4c0cab4ede7bd57
rlc900 <rianna.cleary@gmail.com>
2020-10-22 15:21:57 -0400

    updated managefriends

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+        return (
+            {
+                ...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]
+            }
+        )
+        case "REMOVE_FRIEND":
+            let foundIndex = state.friends.find(friend => friend.id === action.id)
+            return (
+                {
+                    ...state,
+                    friends: [
+                        ...state.friends.slice(0, foundIndex),
+                        ...state.friends.slice(foundIndex + 1)
+                    ]
+                }
+            )
+            default:
+                return state;
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,3 @@
 export function managePresents(state, action){
+    
 }
