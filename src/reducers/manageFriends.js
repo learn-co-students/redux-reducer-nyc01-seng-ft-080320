7c28f092150b7d355c064f0d50b29b9b241de4b1
Russell Sutter <russell.sutter@gmail.com>
2020-10-22 17:54:02 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
-}
+export function manageFriends(state = { friends: [] }, action){
+ switch (action.type) {
+    case "ADD_FRIEND":
+      return ( 
+        {...state, friends: [...state.friends, action.friend ] }
+       )
+    case "REMOVE_FRIEND":
+      const index = state.friends.findIndex(friend => action.id === friend.id );
+      return (
+        {...state, friends: [...state.friends.slice( 0, index ), ...state.friends.slice( index + 1 )] }
+        )
+    default: 
+      return state;
+ }
+};
