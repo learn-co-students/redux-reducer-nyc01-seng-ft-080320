68f6ce74276e8ed3938b2bcb7a040dedf095c010
Vincent Collis <vinsanity0050@gmail.com>
2020-10-23 04:28:27 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+
+
+
+export function manageFriends(state = {friend:[]}, action){
+
+  switch(action.type){
+
+    case"ADD_FRIEND":
+      return({...state, friends: [...state.friends, action.friend]})
+      break;
+    case"REMOVE_FRIEND":
+       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+       return(
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }  
+         )
+      break;
+      default:
+      return state;
+  }
 }
